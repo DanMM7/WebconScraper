7dea896ddea429bdba63e0812d46a75a946df274
Dan <dmalengela@yahoo.com>
2022-11-21 10:25:24 +0200

    Edited views.py formatting error

--- scraper/views.py
+++ scraper/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .helpers import get_all_customer_urls
 
+
 # Create your views here.
 def get_customer(request):
     return JsonResponse({'status': 200})
