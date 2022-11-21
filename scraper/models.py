8faf93466e08fbb581d3f0c48be755de599d5993
Dan <dmalengela@yahoo.com>
2022-11-21 10:02:30 +0200

    Migrated the scrape database tables

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,16 @@
+import uuid
 from django.db import models
 
+
 # Create your models here.
+class BaseModel(models.Model):
+    uid = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+class Customers(BaseModel):
+    customer_name = models.TextField(max_length=100)
+    customer_url = models.URLField()
